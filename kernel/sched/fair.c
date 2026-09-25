8b295befcf37ea0e3e6c98e425d55bf72a374aad
juniarafi213 <juniarafi506@gmail.com>
2026-09-25 19:18:29 +0700

    sched/cass: Introduce the Capacity Aware Superset Scheduler
    The Capacity Aware Superset Scheduler (CASS) optimizes runqueue selection of
    CFS tasks. By using CPU capacity as a basis for comparing the relative
    utilization between different CPUs, CASS fairly balances load across CPUs of
    varying capacities. This results in improved multi-core performance,
    especially when CPUs are overutilized because CASS doesn't clip a CPU's
    utilization when it eclipses the CPU's capacity.
    
    As a superset of capacity aware scheduling, CASS implements a hierarchy of
    criteria to determine the better CPU to wake a task upon between CPUs that
    have the same relative utilization. This way, single-core performance,
    latency, and cache affinity are all optimized where possible.
    
    CASS doesn't feature explicit energy awareness but its basic load balancing
    principle results in decreased overall energy, often better than what is
    possible with explicit energy awareness. By fairly balancing load based on
    relative utilization, all CPUs are kept at their lowest P-state necessary to
    satisfy the overall load at any given moment.
    
    Includes optimizations and fixes:
    - Adapted for msm-4.9 and integrated with Utilization Clamping (UCLAMP).
    - Avoid suboptimal task placement and disproportionate frequency scaling when uclamp is active.
    - Honor uclamp requirements even when candidate CPUs are under thermal pressure.
    - Accurately treat synchronized waker CPUs as idle only when one task is running.
    - Cache affinity, CPU capacity scaling, and low exit latency optimizations.
    - Enable CONFIG_SCHED_CASS in all Tama and SDM845 defconfigs.
    
    Co-developed-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11867,6 +11867,17 @@ static unsigned int get_rr_interval_fair(struct rq *rq, struct task_struct *task
 	return rr_interval;
 }
 
+#ifdef CONFIG_SCHED_CASS
+#include "cass.c"
+
+/* Use CASS. A dummy wrapper ensures the replaced function is still "used". */
+static inline void *select_task_rq_fair_dummy(void)
+{
+	return (void *)select_task_rq_fair;
+}
+#define select_task_rq_fair cass_select_task_rq_fair
+#endif /* CONFIG_SCHED_CASS */
+
 /*
  * All the scheduling class methods:
  */
@@ -11962,6 +11973,9 @@ __init void init_sched_fair_class(void)
 #endif
 #endif /* SMP */
 
+#ifdef CONFIG_SCHED_CASS
+	pr_info("CASS: Capacity Aware Superset Scheduler initialized\n");
+#endif
 }
 
 /* WALT sched implementation begins here */
